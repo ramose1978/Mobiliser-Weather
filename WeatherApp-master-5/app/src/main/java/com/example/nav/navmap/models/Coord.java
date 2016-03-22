96143fff609d048da106bb36d5973cc630c68280
Edwin Ramos <eramos@peritusmobile.co.uk>
2016-03-22 18:05:48 +0000

    Update to Coord models

---
+++ WeatherApp-master-5/app/src/main/java/com/example/nav/navmap/models/Coord.java
@@ -0,0 +1,32 @@
+package com.example.nav.navmap.models;
+
+/**
+ * Created by mobiliser on 20/10/16.
+ */
+public class Coord {
+    
+    private String lon;
+
+    private String lat;
+
+    public String getLon() {
+        return lon;
+    }
+
+    public void setLon(String lon) {
+        this.lon = lon;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    @Override
+    public String toString() {
+        return "ClassPojo [lon = " + lon + ", lat = " + lat + "]";
+    }
+}
