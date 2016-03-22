0a0d7c8e38b9f3e6b23f4c838b1c99ab25b557c1
Edwin Ramos <eramos@peritusmobile.co.uk>
2016-03-22 18:06:37 +0000

    Update to GeographicInformation model class

---
+++ WeatherApp-master-5/app/src/main/java/com/example/nav/navmap/models/GeographicInformation.java
@@ -0,0 +1,12 @@
+package com.example.nav.navmap.models;
+
+/**
+ * Created by mobilser on 20/10/16.
+ */
+public class GeographicInformation {
+    public String city;
+    public String state;
+    public String zip;
+    public String country;
+    public String countryCode;
+}
