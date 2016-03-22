f8d923528a95e7395dbfd79fbba20942baab2321
Edwin Ramos <eramos@peritusmobile.co.uk>
2016-03-22 16:33:09 +0000

    ServiceLibrary public interfaces

---
+++ WeatherApp-master-5/serviceslibrary/src/main/java/com/example/nav/serviceslibrary/NetworkServicesInterface.java
@@ -0,0 +1,13 @@
+package com.example.nav.serviceslibrary;
+
+/**
+ * Created by teammobiliser on 3/10/16.
+ */
+
+public interface NetworkServicesInterface {
+    /**
+     * Returns the result of async network call.
+     * @param string
+     */
+    public void onResult(String string);
+}
