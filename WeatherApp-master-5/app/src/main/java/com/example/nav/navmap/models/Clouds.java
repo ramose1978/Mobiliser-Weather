8d7a1957d771cd62bf22da49cfedc1cee3bccb93
Edwin Ramos <eramos@peritusmobile.co.uk>
2016-03-22 18:02:47 +0000

    Models - update
    Update to models for data object

---
+++ WeatherApp-master-5/app/src/main/java/com/example/nav/navmap/models/Clouds.java
@@ -0,0 +1,13 @@
+package com.example.nav.navmap.models;
+
+/**
+ * Created by mobiliser on 3/10/16.
+ */
+public class Clouds {
+    private String all;
+
+    public String getAll() {
+        return all;
+    }
+
+}
